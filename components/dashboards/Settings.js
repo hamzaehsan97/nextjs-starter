5599f3deff90e0dc05c3364c7f1825b6a0f2e6eb
Hamza Ehsan <hamzaehsan75@gmail.com>
2022-06-15 11:36:14 +0000

    dash view setup

--- components/dashboards/Settings.js
+++ components/dashboards/Settings.js
@@ -1,9 +1,29 @@
-import react from "react";
+import react, { useState, useEffect, useRef } from "react";
+import { useAuth } from "../../context/authContext";
 
 const Settings = () => {
+  const { changeProfilePhoto } = useAuth();
+  const ref = useRef(null);
+
+  const [file, setFile] = useState(null);
+
+  const onChange = (e) => {
+    console.log(ref.current.value);
+    setFile(e.target.files[0]);
+  };
+
+  const handleProfilePhotoChange = async (event) => {
+    changeProfilePhoto(file);
+  };
   return (
     <div>
       <h1>Settings</h1>
+
+      <div>
+        <h1>Dashboard</h1>
+        <input ref={ref} type="file" defaultValue={null} onChange={onChange} />
+        <button onClick={handleProfilePhotoChange}>Upload</button>
+      </div>
     </div>
   );
 };
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,88 +1,88 @@
 import react, { useState } from "react";
 import { useAuth } from "../context/authContext";
 import { useEffect } from "react";
-import Grid from "@mui/material/Grid";
+// import Grid from "@mui/material/Grid";
 import { Drawer, Box } from "@mui/material";
 import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-
+// import List from "@mui/material/List";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import ListItemText from "@mui/material/ListItemText";
+// import InboxIcon from "@mui/icons-material/MoveToInbox";
+import Home from "../components/dashboards/Home";
+import Settings from "../components/dashboards/Settings";
 const drawerWidth = 240;
 const pages = ["home", "dashboard", "profile", "settings"];
 
 const dashboard = () => {
   const [pageView, setPageView] = useState("home");
-  const { user, changeProfilePhoto } = useAuth();
+  const { user } = useAuth();
 
-  // useEffect(() => {
-  //   return () => {};
-  // }, [user]);
-  const [file, setFile] = useState(null);
+  useEffect(() => {
+    return () => {};
+  }, []);
 
-  function handleChange(event) {
-    setFile(event.target.files[0]);
-  }
-
-  const handleProfilePhotoChange = async (event) => {
-    changeProfilePhoto(file);
+  const DashboardSwitch = () => {
+    switch (pageView) {
+      case "home":
+        return <Home />;
+      case "dashboard":
+        return "Dashboard";
+      case "profile":
+        return "Profile";
+      case "settings":
+        return <Settings />;
+      default:
+        return <Home />;
+    }
+  };
+  const changeView = (view) => {
+    setPageView(view);
   };
 
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="space-between"
-      alignItems="flex-start"
-    >
-      <Grid item>
-        <Drawer
-          variant="permanent"
-          className="dash-drawer"
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            [`& .MuiDrawer-paper`]: {
+    <>
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <div style={{ zIndex: 1 }}>
+          <Drawer
+            variant="permanent"
+            className="dash-drawer"
+            sx={{
               width: drawerWidth,
-              boxSizing: "border-box",
-              zIndex: "-1",
-            },
-          }}
-        >
+              flexShrink: 0,
+              [`& .MuiDrawer-paper`]: {
+                width: drawerWidth,
+                boxSizing: "border-box",
+                zIndex: "-1",
+              },
+            }}
+          >
+            <Toolbar />
+            <Box sx={{ overflow: "auto", paddingTop: 20 + "px" }}>
+              <ul>
+                {" "}
+                {pages.map((view) => (
+                  <div key={view}>
+                    <li
+                      onClick={() => {
+                        changeView(view);
+                      }}
+                    >
+                      {view}
+                    </li>
+                  </div>
+                ))}
+              </ul>
+            </Box>
+          </Drawer>
+        </div>
+        <div style={{ zIndex: 1 }}>
           <Toolbar />
-          <Box sx={{ overflow: "auto", paddingTop: 20 + "px" }}>
-            <List>
-              {pages.map((page) => (
-                <ListItem key={page} disablePadding>
-                  <ListItemButton
-                    onClick={() => {
-                      setPageView({ page });
-                      console.log("Changes were made");
-                    }}
-                  >
-                    <ListItemIcon>
-                      <InboxIcon />
-                    </ListItemIcon>
-                    <ListItemText primary={page} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-          </Box>
-        </Drawer>
-      </Grid>
-      <Grid item style={{ marginTop: 300 + "px" }}>
-        {/* <div>
-          <h1>Dashboard</h1>
-          <input type="file" onChange={(e) => setFile(e.target.files[0])} />
-          <button onClick={handleProfilePhotoChange}>Upload</button>
-        </div> */}
-        {/* <h1> {pageView}</h1> */}
-      </Grid>
-    </Grid>
+          <DashboardSwitch pageView={pageView} />
+        </div>
+      </div>
+    </>
   );
 };
 export default dashboard;
