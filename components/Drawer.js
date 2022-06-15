2e27c3cbcdc66f2e3da77da168c8908bdf711cd3
Hamza Ehsan <hamzaehsan75@gmail.com>
2022-06-15 02:20:41 +0000

    One file routing solution

--- components/Drawer.js
+++ components/Drawer.js
@@ -1,50 +1,50 @@
-import { Drawer, Box } from "@mui/material";
-import AppBar from "@mui/material/AppBar";
-import CssBaseline from "@mui/material/CssBaseline";
-import Toolbar from "@mui/material/Toolbar";
-import List from "@mui/material/List";
-import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import Home from "./dashboards/Home";
-import { useContext } from "react";
+// import { Drawer, Box } from "@mui/material";
+// import AppBar from "@mui/material/AppBar";
+// import CssBaseline from "@mui/material/CssBaseline";
+// import Toolbar from "@mui/material/Toolbar";
+// import List from "@mui/material/List";
+// import Typography from "@mui/material/Typography";
+// import Divider from "@mui/material/Divider";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemButton from "@mui/material/ListItemButton";
+// import ListItemIcon from "@mui/material/ListItemIcon";
+// import ListItemText from "@mui/material/ListItemText";
+// import InboxIcon from "@mui/icons-material/MoveToInbox";
+// import Home from "./dashboards/Home";
+// import { useContext } from "react";
 
-const drawerWidth = 240;
+// const drawerWidth = 240;
 
-const DrawerComponent = ({ children }) => {
-  return (
-    <Drawer
-      variant="permanent"
-      className="dash-drawer"
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        [`& .MuiDrawer-paper`]: {
-          width: drawerWidth,
-          boxSizing: "border-box",
-          zIndex: "-1",
-        },
-      }}
-    >
-      <Toolbar />
-      <Box sx={{ overflow: "auto", paddingTop: 20 + "px" }}>
-        <List>
-          <ListItem disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                <InboxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Home" />
-            </ListItemButton>
-          </ListItem>
-        </List>
-      </Box>
-    </Drawer>
-  );
-};
+// const DrawerComponent = ({ children }) => {
+//   return (
+//     <Drawer
+//       variant="permanent"
+//       className="dash-drawer"
+//       sx={{
+//         width: drawerWidth,
+//         flexShrink: 0,
+//         [`& .MuiDrawer-paper`]: {
+//           width: drawerWidth,
+//           boxSizing: "border-box",
+//           zIndex: "-1",
+//         },
+//       }}
+//     >
+//       <Toolbar />
+//       <Box sx={{ overflow: "auto", paddingTop: 20 + "px" }}>
+//         <List>
+//           <ListItem disablePadding>
+//             <ListItemButton>
+//               <ListItemIcon>
+//                 <InboxIcon />
+//               </ListItemIcon>
+//               <ListItemText primary="Home" />
+//             </ListItemButton>
+//           </ListItem>
+//         </List>
+//       </Box>
+//     </Drawer>
+//   );
+// };
 
-export default DrawerComponent;
+// export default DrawerComponent;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,16 +1,26 @@
 import react, { useState } from "react";
 import { useAuth } from "../context/authContext";
 import { useEffect } from "react";
-import Drawer from "../components/Drawer";
 import Grid from "@mui/material/Grid";
-import DashProvider from "../context/dashProvider";
+import { Drawer, Box } from "@mui/material";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+
+const drawerWidth = 240;
+const pages = ["home", "dashboard", "profile", "settings"];
 
 const dashboard = () => {
+  const [pageView, setPageView] = useState("home");
   const { user, changeProfilePhoto } = useAuth();
 
-  useEffect(() => {
-    return () => {};
-  }, [user]);
+  // useEffect(() => {
+  //   return () => {};
+  // }, [user]);
   const [file, setFile] = useState(null);
 
   function handleChange(event) {
@@ -29,14 +39,48 @@ const dashboard = () => {
       alignItems="flex-start"
     >
       <Grid item>
-        <Drawer />
+        <Drawer
+          variant="permanent"
+          className="dash-drawer"
+          sx={{
+            width: drawerWidth,
+            flexShrink: 0,
+            [`& .MuiDrawer-paper`]: {
+              width: drawerWidth,
+              boxSizing: "border-box",
+              zIndex: "-1",
+            },
+          }}
+        >
+          <Toolbar />
+          <Box sx={{ overflow: "auto", paddingTop: 20 + "px" }}>
+            <List>
+              {pages.map((page) => (
+                <ListItem key={page} disablePadding>
+                  <ListItemButton
+                    onClick={() => {
+                      setPageView({ page });
+                      console.log("Changes were made");
+                    }}
+                  >
+                    <ListItemIcon>
+                      <InboxIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={page} />
+                  </ListItemButton>
+                </ListItem>
+              ))}
+            </List>
+          </Box>
+        </Drawer>
       </Grid>
-      <Grid item>
+      <Grid item style={{ marginTop: 300 + "px" }}>
         {/* <div>
           <h1>Dashboard</h1>
           <input type="file" onChange={(e) => setFile(e.target.files[0])} />
           <button onClick={handleProfilePhotoChange}>Upload</button>
         </div> */}
+        {/* <h1> {pageView}</h1> */}
       </Grid>
     </Grid>
   );
